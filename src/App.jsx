4f14b8c266aba002b7deaf1517e8681274eca440
sheyla astorga <sheylaastorga1998@gmail.com>
2024-01-26 10:37:48 -0300

    agregue los import correspondientes a los componentes que agregue de lista de tareas y el conteiner que incluira todos estos componenetes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import { Container } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import FormularioTarea from "./components/FormularioTarea";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Container className="my-4 border border-3 w-50">
+        <h2 className="text-center">Bienvenido</h2>
+        <h5 className="text-center mb-3">Ingresa tus tareas</h5>
+        <FormularioTarea></FormularioTarea>
+      </Container>
+    </>
+  );
 }
 
 export default App;
